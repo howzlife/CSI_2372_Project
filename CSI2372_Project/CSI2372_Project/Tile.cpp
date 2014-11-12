a4f418a2711ef3beb3a322bf2c567080ae1c8ec6
Nick Dubus <nico_dubus@hotmail.com>
2014-11-11 23:35:18 -0500

    added a bunch of stuff, just.... check it out

--- CSI2372_Project/CSI2372_Project/Tile.cpp
+++ CSI2372_Project/CSI2372_Project/Tile.cpp
@@ -7,6 +7,8 @@
 //
 
 #include "Tile.h"
+#include <stdlib.h>
+using namespace std;
 
 void Tile::desert() {
     
@@ -22,17 +24,13 @@ class Restaurant:Tile {
 class Spice_Merchant:Tile {
     bool action( Player& player) {
         if (player.gold >= 2 && (player.spice + player.ruby
-                                 + player.spice + player.jewel) < 9) {
+                                 + player.fabric + player.jewel) < player.cart) {
             player.gold -= 2;
-            switch (player.spice + player.ruby + player.fabric + player.jewel) {
-                case 8:
-                    player.spice ++;
-                    break;
-                case 7:
-                    player.spice += 2;
-                    break;
-                default:
-                    player.spice += 3;
+            player.spice += 3;
+            if ((player.spice + player.ruby
+                 + player.fabric + player.jewel) > player.cart) {
+                player.spice -= ((player.spice + player.ruby
+                                  + player.fabric + player.jewel) - player.cart );
             }
             return true;
         }
@@ -43,20 +41,99 @@ class Spice_Merchant:Tile {
 class Fabric_Manufactures:Tile {
     bool action( Player& player) {
         if (player.gold >= 2 && (player.spice + player.ruby
-                                 + player.spice + player.jewel) < 9) {
+                                 + player.fabric + player.jewel) < player.cart) {
             player.gold -= 2;
-            switch (player.spice + player.ruby + player.fabric + player.jewel) {
-                case 8:
-                    player.fabric++;
-                    break;
-                case 7:
-                    player.fabric += 2;
-                    break;
-                default:
-                    player.fabric += 3;
+            player.fabric += 3;
+            if ((player.spice + player.ruby
+                 + player.fabric + player.jewel) > player.cart) {
+                player.fabric -= ((player.spice + player.ruby
+                                  + player.fabric + player.jewel) - player.cart );
             }
             return true;
         }
+        return false;
+    }
+};
+
+class Jeweler:Tile {
+    bool action( Player& player) {
+        if (player.gold >= 2 && (player.spice + player.ruby
+                                 + player.fabric + player.jewel) < player.cart) {
+            player.gold -= 2;
+            player.jewel += 3;
+            if ((player.spice + player.ruby
+                 + player.fabric + player.jewel) > player.cart) {
+                player.jewel -= ((player.spice + player.ruby
+                                  + player.fabric + player.jewel) - player.cart );
+            }
+            return true;
+        }
+        return false;
+    }
+};
+
+class Cart_Manufacturer:Tile {
+    bool action( Player& player) {
+        if (player.gold >= 7) {
+            player.gold -= 7;
+            player.cart += 3;
+            return true;
+        }
+        return false;
+    }
+};
+
+class Small_Market:Tile {
+    bool action( Player& player) {
+        if (player.fabric > 0 && player.jewel > 0
+            && player.spice > 0) {
+            player.fabric --;
+            player.jewel --;
+            player.spice --;
+            player.gold += 8;
+            return true;
+        }
+        return false;
+    }
+};
+
+class Spice_Market:Tile {
+    bool action( Player& player) {
+        if ( player.spice >= 3) {
+            player.spice -= 3;
+            player.gold += 6;
+            return true;
+        }
+        return false;
+    }
+};
+
+class Jewelry_Market:Tile {
+    bool action( Player& player) {
+        if ( player.jewel >= 3) {
+            player.jewel -= 3;
+            player.gold += 6;
+            return true;
+        }
+        return false;
+    }
+};
+
+class Fabric_Market:Tile {
+    bool action( Player& player) {
+        if ( player.fabric >= 3) {
+            player.fabric -= 3;
+            player.gold += 6;
+            return true;
+        }
+        return false;
+    }
+};
+
+class Black_Market:Tile {
+    bool action( Player& player) {
+        int v1 = rand() % 6;
+        int v2 = rand() % 5;
         return false;
     }
 };
